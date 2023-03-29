838435df2909690874b97d5cd1ff019ad177b59f
aiden <aidenrphotos@gmail.com>
2023-03-29 12:32:23 -0400

    Spawn ball

---
+++ ball.py
@@ -0,0 +1,4 @@
+import pygame
+
+moving_ball = pygame.Rect(494, 290, 10, 10)
+x_speed, y_speed = 6, 5
--- main.py
+++ main.py
@@ -1,27 +1,51 @@
 import pygame
-import math
+import ball
 
-background_color = (1, 1, 1)
-screen = pygame.display.set_mode((1000, 600))
+
+# Make ball move
+def move_ball():
+    global x_speed, y_speed
+    moving_ball.x += x_speed
+    moving_ball.y += y_speed
+
+    # Collision with top and bottom screen
+    if moving_ball.bottom >= screen_height or moving_ball.top <= 0:
+        y_speed *= -1
+
+    pygame.draw.rect(screen, white, moving_ball)
+
+
+moving_ball = ball.moving_ball
+x_speed = ball.x_speed
+y_speed = ball.y_speed
+
+pygame.init()
+clock = pygame.time.Clock()
+screen_width = 1000
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pong")
-screen.fill(background_color)
+screen.fill((5, 5, 5))
 
-white = (255, 255, 255)
+white = (200, 200, 200)
 
-running = True
 
-while running:
+# Draw vertical dotted line in center of screen
+def draw_dotted_line():
+    for i in range(32, 590, 5):
+        if i % 2 == 0:
+            pygame.draw.line(screen, white, (499, i - 10), (499, i), 5)
+        if i % 2 != 0:
+            pygame.draw.line(screen, (5, 5, 5), (499, i - 10), (499, i), 5)
+
+
+draw_dotted_line()
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
 
-    def draw_dotted_line():
-        for i in range(32, 590, 5):
-            if i % 2 == 0:
-                pygame.draw.line(screen, (255, 255, 255), (499, i - 10), (499, i), 5)
-            if i % 2 != 0:
-                pygame.draw.line(screen, (0, 0, 0), (499, i - 10), (499, i), 5)
-
-    draw_dotted_line()
-
+    move_ball()
     pygame.display.flip()
+    clock.tick(60)
